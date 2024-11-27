f0cb7ef40755f0b8dd00b927d3c088e7777ef21c
Kitty-beeb <kitthas@gmail.com>
2024-11-27 18:10:27 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ const colors = [
     "yellow",
 ];
 
-// Tetrispalojen värikkäät kehut Delille
+// Suomalaiset kehut Delille
 const compliments = [
     "Del, olet mahtava!",
     "Hienoa, Del! Olet loistava!",
@@ -90,6 +90,42 @@ function createPiece(type) {
             [0, 1, 1],
             [0, 0, 0],
         ];
+    }
+}
+
+// Pyöritä palikkaa
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
+        }
+    }
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
+
+// Pelaajan palikan pyöritys
+function playerRotate(dir) {
+    const pos = player.pos.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+    while (collide(arena, player)) {
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if (offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
     }
 }
 
@@ -226,6 +262,10 @@ document.addEventListener("keydown", event => {
         playerMove(1);
     } else if (event.key === "ArrowDown") {
         playerDrop();
+    } else if (event.key === "q") {
+        playerRotate(-1);
+    } else if (event.key === "w") {
+        playerRotate(1);
     }
 });
 
